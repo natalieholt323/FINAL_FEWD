92892ce1c6bceaf24921c92b449a5eea7a1339c3
Natalie <natalieholt323@gmail.com>
2016-03-06 16:58:36 -0500

    made div close!

--- js/index.js
+++ js/index.js
@@ -1,12 +1,31 @@
 
+
 $(".pin1Ex img").click(function(){
-    $(".profile_box").show();
+    $(".box").show();
 });
 
 $(".pin2Ex img").click(function(){
-    $(".profile_box").show();
+    $(".box").show();
+
 });
 
+$('#boxclose').click(function(){
+    $(".box").hide();
+        });
+
 $(".pin4Ex img").click(function(){
-    $(".profile_box").show();
+    $(".box").show();
 });
+/*
+$('.pin2Ex img').click(function(){
+        $('#overlay').fadeIn(200,function(){
+            $('#box').animate({'top':'20px'},200);
+        });
+        return false;
+    });
+    $('#boxclose').click(function(){
+        $('#profile_box').animate({'top':'-200px'},500,function(){
+            $('#overlay').fadeOut('fast');
+        });
+    });
+ */
