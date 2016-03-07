382da403a0f1a0cf166e48c38df4965d37360cf3
Natalie <natalieholt323@gmail.com>
2016-03-06 20:23:21 -0500

    added second box

--- js/index.js
+++ js/index.js
@@ -1,8 +1,6 @@
 
 
-$(".pin1Ex img").click(function(){
-    $(".box").show();
-});
+
 
 $(".pin2Ex img").click(function(){
     $(".box").show();
@@ -14,7 +12,7 @@ $('#boxclose').click(function(){
         });
 
 $(".pin4Ex img").click(function(){
-    $(".box").show();
+    $(".box2").show();
 });
 /*
 $('.pin2Ex img').click(function(){
