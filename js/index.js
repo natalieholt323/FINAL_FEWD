abbaa44e9ab96c65a4cd7a12dd09b4411f45eb67
Natalie <natalieholt323@gmail.com>
2016-03-10 16:34:36 -0500

    fixed box closing problem

--- js/index.js
+++ js/index.js
@@ -1,20 +1,38 @@
 
 
-
-
 $(".pin2Ex img").click(function(){
     $(".box").show();
 
 });
-
-$('#boxclose').click(function(){
-    $(".box").hide();
-        });
-
+			
 $(".pin4Ex img").click(function(){
     $(".box2").show();
 });
+					
+$('.boxclose').click(function(){
+	console.log('ss');
+	$(this).parent().hide();
+    // $(".box").show();
+});
+		
+// $('#boxclose').click(function(){
+// 	console.log('clicked');
+//     // $(".box2").hide();
+//  });
+
+
 /*
+var navOpen = false;
+$('.hamburger').on('click', function(){
+	if (navOpen) {
+		$('#page-header').animate({height:45px})
+		//animate height of header closed - 45px
+	
+
+}else {
+	$('#page-header').animate({height:240},2000);s
+}
+}
 $('.pin2Ex img').click(function(){
         $('#overlay').fadeIn(200,function(){
             $('#box').animate({'top':'20px'},200);
